50f35c1543987e98a7b2592697e72f5afbf1bc64
Daniil Kornach <danik.kornach@gmail.com>
2023-05-20 16:01:20 +0300

    Изменения: теперь программа выводит только количество четных чисел.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-	setlocale(LC_ALL, "Russian");
-	char String[] = "Hello 2023 year. ~3256Ёё123";
-	int count = 0;
-	for (int i = 0; String[i]; i++) {
-		if (((int)String[i] & 1) == 0) {
-			cout << (int)String[i] << ' ';
-			count++;
-		}
-	}
-	cout << "Количество четных: " << count << endl;
-	return 0;
+    using namespace std;
+    setlocale(0, "rus");
+    char string[150] = "Hello 2023 year. ~3256Ёё123";
+    int count = 0;
+
+    for (int i = 0; string[i]; i++) {
+        if (isdigit(string[i]) != 0) {
+            if (((string[i] - '0') & 1) == 0) {
+                count++;
+            }
+
+        }
+    }
+
+    cout << "Количество чётных чисел: " << count << endl;
+
+    return 0;
 }

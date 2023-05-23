b25d3d3fa9046aadeec29f608517f8c29ef1aa0f
Daniil Kornach <danik.kornach@gmail.com>
2023-05-23 10:46:54 +0300

    Оптимизировал программу.

--- main.cpp
+++ main.cpp
@@ -4,14 +4,13 @@ int main() {
     using namespace std;
     setlocale(0, "rus");
     char string[] = "Hello 2023 year. ~3256¨¸123";
+    char* pstring;
     int count = 0;
+    pstring = string;
 
-    for (int i = 0; string[i]; i++) {
-        if (isdigit(string[i]) != 0) {
-            if (((string[i] - '0') & 1) == 0) {
-                count++;
-            }
-
+    for (int i = 0; *(pstring + i); i++) {
+        if (isdigit(*(pstring + i)) && (*(pstring + i) & 1) == 0) {
+            count++;
         }
     }
 

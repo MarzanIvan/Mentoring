133e2428ce00f54019a36b23da556cde6bb62c58
Daniil Kornach <danik.kornach@gmail.com>
2023-05-26 10:54:08 +0300

    "pstring" стал локальным в "for" т.к не испульзуется вне "for".

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
+#include <cctype>
 
 int main() {
     using namespace std;
     setlocale(0, "rus");
     char string[] = "Hello 2023 year. ~3256¨¸123";
-    char* pstring;
     int count = 0;
-    pstring = string;
 
-    for (int i = 0; *(pstring + i); i++) {
-        if (isdigit(*(pstring + i)) && (*(pstring + i) & 1) == 0) {
+
+    for (char* pstring = string; *(pstring++); ) {
+        if (isdigit(*pstring) && (*pstring & 1) == 0) {
             count++;
         }
     }

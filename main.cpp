087f505f2e1ce57f652b2113034d77b6a5b9f125
Daniil Kornach <danik.kornach@gmail.com>
2023-06-07 12:37:46 +0300

    Выполнил с инструкцией "sort" и диапазонным циклом.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
-#include <cctype>
+#include <algorithm>
+using namespace std;
 
 int main() {
-    using namespace std;
-    setlocale(0, "rus");
-    char string[] = "Hello 2023 year. ~3256Ёё123";
-    int count = 0;
+	char string[] = { "JGMXCIJPAINKALNTHQNMA" };
+	int size = sizeof(string) - 1;
+	sort(string, string + size);
+	cout << "Result: ";
 
-
-    for (char* pstring = string; *(pstring++); ) {
-        if (isdigit(*pstring) && (*pstring & 1) == 0) {
-            count++;
-        }
-    }
-
-    cout << "Количество чётных чисел: " << count << endl;
-
-    return 0;
+	for (char letter : string) {
+		cout << letter;
+	}
+	return 0;
 }

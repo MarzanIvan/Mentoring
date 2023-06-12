50adbdc519278a0effe823585940700792b30ad6
Daniil Kornach <danik.kornach@gmail.com>
2023-06-12 12:39:27 +0300

    Сделал пузырьковым методом с помощью индексацией массива.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main() {
 	char string[] = { "JGMXCIJPAINKALNTHQNMA" };
-	int size = sizeof(string) - 1;
-	sort(string, string + size);
+	int size = sizeof(string);
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < (size - 1); j++) {
+			if (string[j] > string[j + 1]) {
+				char letter = string[j];
+				string[j] = string[j + 1];
+				string[j + 1] = letter;
+			}
+		}
+	}
 	cout << "Result: ";
-
-	for (char letter : string) {
-		cout << letter;
+	for (int i = 0; i < size; i++) {
+		cout << string[i];
 	}
 	return 0;
 }
